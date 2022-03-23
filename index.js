672692b61bf6809df970acad13dab28986ccfe33
Igor M. B <nicklink.lv@gmail.com>
2022-03-22 21:00:06 -0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('vamos começar!'));
+
+const paragrafo = 'Texto retornado por uma função';
+
+
+function text(sting) {
+    return sting;
+}
+
+console.log(text(paragrafo));

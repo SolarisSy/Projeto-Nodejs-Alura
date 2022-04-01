91001a2917721991a7f4fb7469d5c142cde9519c
Igor M. B <nicklink.lv@gmail.com>
2022-03-31 23:06:49 -0300

    new exercises

--- 2299-lib-nodejs-markdown-aula-6/test/index.test.js
+++ 2299-lib-nodejs-markdown-aula-6/test/index.test.js
@@ -11,17 +11,17 @@ describe('pegaArquivo::', () => {
     expect(typeof pegaArquivo).toBe('function');
   })
   it('deve retornar array com resultados', async () => {
-    const resultado = await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos/texto1.md') 
+    const resultado = await pegaArquivo('G:\HD 1 TB\ARQUIVOS\Programing Projects\JS\Projeto-Nodejs-Alura\2299-lib-nodejs-markdown-aula-6\test\arquivos\texto1.md') 
     expect(resultado).toEqual(arrayResult)
   })
   it('deve retornar mensagem "não há links"', async () => {
-    const resultado = await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos/texto1_semlinks.md')
+    const resultado = await pegaArquivo('G:\HD 1 TB\ARQUIVOS\Programing Projects\JS\Projeto-Nodejs-Alura\2299-lib-nodejs-markdown-aula-6\test\arquivos\texto1_semlinks.md')
     expect(resultado).toBe('não há links');
   })
   it('deve lançar um erro na falta de arquivo', () => {
     async function capturaErro() {
-      await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos')
+      await pegaArquivo('G:\HD 1 TB\ARQUIVOS\Programing Projects\JS\Projeto-Nodejs-Alura\2299-lib-nodejs-markdown-aula-6\test\arquivos')
       expect(capturaErro).toThrowError(/não há arquivo no caminho/)
     }
-  })
+  })  
 })

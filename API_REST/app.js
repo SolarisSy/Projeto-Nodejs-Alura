2b859db832e05687de0a6dce09c0206fb79cb86e
Igor M. B <nicklink.lv@gmail.com>
2022-04-27 13:15:17 -0300

    w31

---
+++ API_REST/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+const livros = [
+    {id: 1,"titulo": "Senhor dos Aneis"},
+    {id: 2, "titulo": "O Hobbit"}
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node');
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso')
+})
+ 
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
+export default app
---
+++ Server js/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    const valor = req.query.dadoExterno
+    const valorDoResponse = `Você me mandou tal ${valor}`;
+    res.send(valorDoResponse)
+})
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`
+    Servidor subiu com sucesso!
+    http://localhost:${port}
+    `)
+})
